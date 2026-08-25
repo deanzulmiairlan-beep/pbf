d01475ed776eeaf7500463db626833429b4ed1e4
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 17:03:34 +0700

    Update AppFunc.jsx

--- PBF-3/src/AppFunc.jsx
+++ PBF-3/src/AppFunc.jsx
@@ -1,12 +1,20 @@
+// Mengimpor file CSS App.css untuk memberikan style pada komponen
 import './App.css'
 
+// Membuat dan mengekspor function component bernama AppFunc
 export default function AppFunc() {
 
+  // Membuat elemen JSX berupa heading dengan teks Hello, world
   const element = <h1>Hello, world</h1>;
 
+  // Mengembalikan tampilan yang akan ditampilkan oleh komponen AppFunc
   return (
     <main>
+
+      {/* Menampilkan heading tingkat 2 */}
       <h2>Belajar menggunakan JSX pada React</h2>
+
+      {/* Memanggil dan menampilkan komponen Header */}
       <Header />
 
     </main>
@@ -14,23 +22,44 @@ export default function AppFunc() {
 }
 
 
+// Membuat function component bernama Header
 function Header() {
+
+  // Mengembalikan tampilan dari komponen Header
   return (
     <div>
+
+      {/* Menampilkan judul utama */}
       <h1>Tutorial Reactjs untuk Pemula</h1>
+
+      {/* Menampilkan informasi bahwa ini menggunakan Function Component */}
       <h2>Function Component</h2>
+
+      {/* Memanggil komponen Message dan mengirimkan data melalui props */}
       <Message sender="dian" content="Hi, Apa kabar?" />
+
     </div>
   );
 }
 
+
+// Membuat function component bernama Message
+// Props digunakan untuk menerima data dari komponen lain
 function Message(props) {
+
+  // Mengembalikan tampilan pesan
   return (
     <div>
+
+      {/* Menampilkan nilai sender dari props */}
       <small>{props.sender}:</small>
+
+      {/* Menampilkan isi pesan dari props */}
       <p>{props.content}</p>
+
+      {/* Membuat garis horizontal */}
       <hr />
+
     </div>
   );
-
 }
