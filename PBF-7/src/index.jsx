23e1585e6ce13f5d5ca4d7aa6e17a4d4f6596d6e
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 17:36:14 +0700

    Update index.jsx

--- PBF-7/src/index.jsx
+++ PBF-7/src/index.jsx
@@ -1,9 +1,22 @@
+// Mengimpor library React
 import React from 'react'
+
+// Mengimpor ReactDOM untuk menampilkan aplikasi React ke halaman HTML
 import ReactDOM from 'react-dom/client'
+
+// Mengimpor komponen utama App dari file App
 import App from './App'
 
+
+// Mencari elemen HTML dengan id "root"
+// Kemudian membuat root React dan menampilkan aplikasi
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+
+  // StrictMode digunakan untuk membantu mendeteksi masalah pada aplikasi saat pengembangan
+  <React.StrictMode>
+
+    {/* Menampilkan komponen utama App */}
+    <App />
+
+  </React.StrictMode>
 )
