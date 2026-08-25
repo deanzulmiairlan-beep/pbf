3a4765ab1669cca12f00d54ee859a8e7b18c5c0a
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 17:38:31 +0700

    Update index.jsx

--- PBF-6/src/index.jsx
+++ PBF-6/src/index.jsx
@@ -1,10 +1,25 @@
+// Mengimpor library React
 import React from 'react'
+
+// Mengimpor ReactDOM untuk menampilkan aplikasi React ke halaman HTML
 import ReactDOM from 'react-dom/client'
+
+// Mengimpor komponen App dari file App
 import App from './App'
+
+// Mengimpor komponen AppRouter yang berisi pengaturan halaman dan routing
 import AppRouter from './AppRouter'
 
+
+// Mencari elemen HTML dengan id "root"
+// Kemudian membuat root React sebagai tempat aplikasi ditampilkan
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  // StrictMode digunakan untuk membantu mendeteksi potensi masalah saat pengembangan
   <React.StrictMode>
+
+    {/* Menampilkan komponen AppRouter sebagai komponen utama */}
     <AppRouter />
+
   </React.StrictMode>
 )
