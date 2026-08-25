582fbfdb3432258f97c5b32ae8b94db591bedb0d
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 17:42:13 +0700

    Update AppCustomHook.jsx

--- PBF-5/src/AppCustomHook.jsx
+++ PBF-5/src/AppCustomHook.jsx
@@ -1,15 +1,24 @@
+// Mengimpor file CSS untuk mengatur tampilan aplikasi
 import './App.css'
+
+// Mengimpor komponen UserForm dari folder components
 import UserForm from './components/UserForm'
 
+
+// Membuat dan mengekspor komponen AppCustomHook
 export default function AppCustomHook() {
 
+  // Mengembalikan tampilan komponen
   return (
+    // Elemen main digunakan sebagai bagian utama halaman
     <main>
+
+      {/* Menampilkan judul utama halaman */}
       <h1>Belajar React Custom Hook</h1>
 
+      {/* Memanggil dan menampilkan komponen UserForm */}
       <UserForm />
+
     </main>
   )
-
-
 }
