852ee691d37155b21c12b3ca3da17c0707068a50
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 16:45:15 +0700

    Update class.js

--- PBF-01/class.js
+++ PBF-01/class.js
@@ -1,12 +1,27 @@
+// Membuat class bernama Holiday
 class Holiday {
+
+  // Constructor digunakan untuk menerima data tujuan dan jumlah hari
   constructor(destination, days) {
+
+    // Menyimpan nilai tujuan ke dalam properti destination
     this.destination = destination;
+
+    // Menyimpan nilai jumlah hari ke dalam properti days
     this.days = days;
   }
+
+  // Membuat method bernama info
   info() {
+
+    // Menampilkan informasi tujuan dan jumlah hari menggunakan alert
     alert(this.destination + " will take " + this.days + " days.");
   }
 }
 
+// Membuat object bernama trip dari class Holiday
+// Dengan tujuan Semeru dan lama perjalanan 10 hari
 var trip = new Holiday("Semeru", 10);
+
+// Memanggil method info dari object trip
 trip.info();
