bd399ae96cf9a01c012a7df1c02cfee23129a346
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 16:41:04 +0700

    Update array.js

--- PBF-01/array.js
+++ PBF-01/array.js
@@ -1,2 +1,5 @@
+// Membuat variabel products yang berisi beberapa nama produk
 var products = ["Flashdisk", "SDD", "Monitor"];
+
+// Mengambil elemen HTML dengan id "demo" dan menampilkan produk pertama dari array
 document.getElementById("demo").innerHTML = products[0];
