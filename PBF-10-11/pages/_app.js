9d7b7f73d809d380783d5e898cf7bb2ce8c5faff
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 18:10:15 +0700

    Update _app.js

--- PBF-10-11/pages/_app.js
+++ PBF-10-11/pages/_app.js
@@ -1,7 +1,17 @@
+// Mengimpor file CSS global yang digunakan untuk seluruh halaman aplikasi
 import '../styles/globals.css'
 
+
+// Membuat komponen utama MyApp
+// Component berisi komponen halaman yang sedang ditampilkan
+// pageProps berisi data atau props yang dikirimkan ke halaman tersebut
 function MyApp({ Component, pageProps }) {
+
+  // Menampilkan halaman/komponen yang sedang aktif
+  // ...pageProps digunakan untuk meneruskan seluruh props ke Component
   return <Component {...pageProps} />
 }
 
+
+// Mengekspor MyApp sebagai komponen utama aplikasi
 export default MyApp
