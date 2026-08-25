2921a14a7dfdb9b85e861c68fee59614839374fb
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 16:43:30 +0700

    Update arrow.js

--- PBF-01/arrow.js
+++ PBF-01/arrow.js
@@ -1,8 +1,18 @@
+// Membuat fungsi bernama greeting menggunakan arrow function
 var greeting = message => {
+
+  // Menampilkan isi dari parameter message dalam kotak alert
   alert(message);
 }
+
+// Memanggil fungsi greeting dengan mengirimkan pesan "Selamat Pagi"
 greeting('Selamat Pagi');
 
+
+// Membuat fungsi bernama greetingNew menggunakan arrow function
+// Fungsi ini memiliki dua parameter, yaitu message1 dan message
 var greetingNew = (message1, message) => {
+
+  // Menampilkan gabungan pesan menggunakan alert
   alert(message1 + message2);
 }
