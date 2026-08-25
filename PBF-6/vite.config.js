019d66c6a1c2045b8dc697348baec2fd34e443e7
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 17:28:53 +0700

    Update vite.config.js

--- PBF-6/vite.config.js
+++ PBF-6/vite.config.js
@@ -1,10 +1,24 @@
+// Mengimpor fungsi defineConfig dari Vite untuk membuat konfigurasi project
 import { defineConfig } from 'vite';
+
+// Mengimpor plugin React agar Vite dapat mendukung dan menjalankan aplikasi React
 import react from '@vitejs/plugin-react';
 
+// Link dokumentasi konfigurasi Vite
 // https://vitejs.dev/config/
+
+// Mengekspor konfigurasi utama yang digunakan oleh Vite
 export default defineConfig({
+
+  // Menambahkan plugin React ke dalam project
   plugins: [react()],
+
+  // Mengatur konfigurasi server saat project dijalankan
   server: {
+
+    // Mengatur host menjadi 0.0.0.0 agar server dapat diakses melalui jaringan
     host: '0.0.0.0',
+
   }
+
 })
