22d5e37f2578d56a0431b93d1f4f60c23ca5d8af
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 16:50:14 +0700

    Update template.js

--- PBF-01/template.js
+++ PBF-01/template.js
@@ -1,6 +1,14 @@
+// Membuat variabel firstName dan menyimpan nama depan Cristiano
 let firstName = 'Cristiano';
+
+// Membuat variabel lastName dan menyimpan nama belakang Ronaldo
 let lastName = 'Ronaldo';
+
+// Menampilkan kotak input untuk meminta pengguna memasukkan umur Cristiano Ronaldo
 let age = prompt("Masukkan umur Cristiano Ronaldo");
 
+// Membuat kalimat menggunakan template literal dan memasukkan nilai dari variabel
 let result = `${firstName} ${lastName} is ${age} years old`;
+
+// Menampilkan hasil kalimat menggunakan kotak alert
 alert(result)
