64a30c8ea835aa61cd4221f678a99ab68161b890
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 16:51:11 +0700

    Update variable.js

--- PBF-01/variable.js
+++ PBF-01/variable.js
@@ -1,7 +1,17 @@
+// Membuat variabel tanggal menggunakan var dengan nilai 29
 var tanggal = 29;
+
+// Mengubah nilai variabel tanggal menjadi 30
 tanggal = "30";
+
+// Membuat variabel namaBulan menggunakan let dengan nilai September
 let namaBulan = "September";
+
+// Mengubah nilai variabel namaBulan menjadi angka 10
 namaBulan = 10;
+
+// Membuat variabel konstan tahun dengan nilai 2022
 const tahun = "2022";
 
-alert(tanggal + " " + namaBulan + " "+tahun)
+// Menampilkan gabungan nilai tanggal, nama bulan, dan tahun menggunakan alert
+alert(tanggal + " " + namaBulan + " " + tahun)
