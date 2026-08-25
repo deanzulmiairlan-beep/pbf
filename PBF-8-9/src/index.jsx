e0ded1c5a4bb4b811627f9e1b409ab8f183d5342
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 18:06:51 +0700

    Update index.jsx

--- PBF-8-9/src/index.jsx
+++ PBF-8-9/src/index.jsx
@@ -1,13 +1,37 @@
+// Mengimpor library React
 import React from 'react'
+
+// Mengimpor ReactDOM untuk menampilkan aplikasi React ke halaman HTML
 import ReactDOM from 'react-dom/client'
+
+// Mengimpor komponen utama App
 import App from './App'
+
+// Mengimpor Provider dari React Redux
+// Provider digunakan untuk menyediakan Redux Store kepada seluruh komponen aplikasi
 import { Provider } from "react-redux";
+
+// Mengimpor store dari file redux
 import { store } from "./redux";
 
+
+// Membuat root React pada elemen HTML yang memiliki id "root"
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  // StrictMode digunakan untuk membantu mendeteksi potensi masalah
+  // selama proses pengembangan aplikasi
   <React.StrictMode>
+
+    {/* 
+      Provider membungkus komponen App agar seluruh komponen di dalamnya
+      dapat mengakses Redux Store
+    */}
     <Provider store={store}>
+
+      {/* Menampilkan komponen utama aplikasi */}
       <App />
+
     </Provider>
+
   </React.StrictMode>
 )
