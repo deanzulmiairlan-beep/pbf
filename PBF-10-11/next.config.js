bd05e790049d59368f36e3f0c00aabc4a67b49c9
deanzulmiairlan-beep <deanzulmiairlan@gmail.com>
2026-08-25 18:15:13 +0700

    Update next.config.js

--- PBF-10-11/next.config.js
+++ PBF-10-11/next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * Memberikan informasi kepada editor/IDE bahwa objek konfigurasi
+ * ini menggunakan tipe NextConfig dari Next.js.
+ */
 const nextConfig = {
+
+  /**
+   * Mengaktifkan React Strict Mode.
+   * Mode ini membantu mendeteksi potensi masalah dalam aplikasi
+   * selama proses development.
+   */
   reactStrictMode: true,
 }
 
+/**
+ * Mengekspor konfigurasi Next.js agar dapat digunakan
+ * saat aplikasi Next.js dijalankan atau dibangun.
+ */
 module.exports = nextConfig
